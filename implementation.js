52cd62c03c36d74d33f6d0c5ec110573c5b58613
Jordan Harband <ljharb@gmail.com>
2022-11-02 15:24:54 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var Call = require('es-abstract/2021/Call');
-var Get = require('es-abstract/2021/Get');
-var IsCallable = require('es-abstract/2021/IsCallable');
-var LengthOfArrayLike = require('es-abstract/2021/LengthOfArrayLike');
-var ToBoolean = require('es-abstract/2021/ToBoolean');
-var ToObject = require('es-abstract/2021/ToObject');
-var ToString = require('es-abstract/2021/ToString');
+var Call = require('es-abstract/2022/Call');
+var Get = require('es-abstract/2022/Get');
+var IsCallable = require('es-abstract/2022/IsCallable');
+var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
+var ToBoolean = require('es-abstract/2022/ToBoolean');
+var ToObject = require('es-abstract/2022/ToObject');
+var ToString = require('es-abstract/2022/ToString');
 
 module.exports = function findIndex(predicate) {
 	var O = ToObject(this);
